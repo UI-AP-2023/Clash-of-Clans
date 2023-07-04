1773f6515a259a4da07b5fcb4a75c1e36fe3821f
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2023-07-04 03:55:11 +0330

    Added WinPageController and LossPageController classes and their fxml files.

---
+++ Game/src/main/java/com/example/game/LossPageController.java
@@ -0,0 +1,44 @@
+package com.example.game;
+
+import controller.GameController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class LossPageController implements Initializable {
+    private final GameController gameController=new GameController();
+    @FXML
+    private ImageView backImage;
+
+    @FXML
+    void backImage(MouseEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("player-page.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Home");
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            gameController.updateWins((PlayerPageController.opponentPlayer.getNumberOfWins()+1),PlayerPageController.opponentPlayer.getPlayerID());
+            gameController.updateLosses((LoginPageController.loginPlayer.getNumberOfLosses()+1),LoginPageController.loginPlayer.getPlayerID());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
